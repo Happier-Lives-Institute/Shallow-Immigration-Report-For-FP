39275e7f0895a67a3492a0ed231df9045e2f77fc
Karakaii <samu.dupret@hotmail.com>
2022-05-17 20:48:47 +0100

    a lot of new modelling

--- migration_gap.R
+++ migration_gap.R
@@ -71,3 +71,61 @@ ggplot(d.nativespillover, aes(x=attitudes, y=`d adjusted`)) +
 #   # geom_smooth() +
 #   geom_abline() +
 #   theme_cowplot()
+
+GWP <- read_csv("happiness-cantril-ladder.csv", show_col_types = FALSE) %>% 
+  rename(ls = `Life satisfaction in Cantril Ladder (World Happiness Report 2021)`,
+         country = Entity) %>% 
+  mutate(time = Year - 2005)
+
+# Gap between happy and unhappy countries
+
+# Get average before 2010
+GWP00s <- GWP %>% filter(Year < 2010) %>% group_by(country) %>% summarise(
+  pre2010avg = mean(ls, na.rm = T)
+)
+GWP <- full_join(GWP, GWP00s)
+# Get scores after 2010
+# Separate between unhappy and happy
+GWP10s <- GWP %>% filter(Year >= 2010) %>% mutate(unhappy = pre2010avg < 5)
+
+GWP10s.mod1 <- lm(ls ~ unhappy*time, GWP10s)
+GWP10s.mod2 <- lm(log(ls) ~ unhappy*time, GWP10s)
+exp(coef(GWP10s.mod2))
+
+# LS over time analysis
+summary(lm(ls ~ time, GWP))
+summary(lm(log(ls) ~ time, GWP))
+
+# SD of LS over time analysis
+GWP_sd <- GWP %>% group_by(Year) %>% 
+  summarise(sd = sd(ls, na.rm = T)) %>% 
+  mutate(time = Year - 2005)
+GWP.mod1 <- lm(sd ~ time, GWP_sd)
+summary(GWP.mod1)
+GWP.mod2 <- lm(log(sd) ~ time, GWP_sd)
+summary(GWP.mod2)
+exp(coef(GWP.mod2))
+
+
+
+
+hhspillovers <- read_sheet(
+  "https://docs.google.com/spreadsheets/d/13s3TOFG8tlroWkJ_MjAYDyqs7KOj6ZMUREMTAr97W08/",
+  sheet = "Immigrate <> SWB (hh spillovers)"
+)
+
+hhspillovers <- hhspillovers %>% rowwise() %>% mutate(
+  n1 = `sample size`/2,
+  n2 = `sample size`/2,
+  d_se = getDSE(d, n1=n1, n2=n2)
+) %>% ungroup()
+
+mod.hhspillovers1 <- rma.mv(yi = d, V = d_se^2,
+                               slab = Citation,
+                               random =  ~1 | Citation,
+                               test = "t", method="REML",
+                               data = hhspillovers)
+summary(mod.hhspillovers1)
+
+# hhspillovers <- hhspillovers %>% rename(N = `sample size`, gap = `% of gap closed`)
+# GAP.hh <- weighted.mean(x = hhspillovers$gap, w = hhspillovers$N, na.rm = T); GAP.hh*100

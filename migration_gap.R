66ce9c908ff10caeb5414879525415227e8c819a
Karakaii <samu.dupret@hotmail.com>
2022-05-23 09:16:53 +0100

    clean up R script

--- migration_gap.R
+++ migration_gap.R
@@ -1,10 +1,14 @@
 # Read dependencies
 source("_dependencies/dependencies.R")
 
+######################################
+# Calculating the closing of the gap #
+######################################
+
 # Get the data from the gsheet
 d.all <- read_sheet(
   "https://docs.google.com/spreadsheets/d/13s3TOFG8tlroWkJ_MjAYDyqs7KOj6ZMUREMTAr97W08/",
-  sheet = "Immigrate <> SWB (closing the gap)"
+  sheet = "Closing the SWB gap"
 )
 
 # Get data for calculation of how much the gap is closed
@@ -17,10 +21,39 @@ uniqueN(d.gap$Citation) - 2 # number of sources understanding that PINZMS is onl
 GAP <- weighted.mean(x = d.gap$gap, w = d.gap$N); GAP*100
 range(d.gap$gap)*100
 
+######################################################
+# Calculating how LS in countries changes over time  #
+######################################################
+
+GWP <- read_csv("happiness-cantril-ladder.csv", show_col_types = FALSE) %>% 
+  rename(ls = `Life satisfaction in Cantril Ladder (World Happiness Report 2021)`,
+         country = Entity) %>% 
+  mutate(time = Year - 2005)
+
+# Gap between happy and unhappy countries
+
+# Get average before 2010
+GWP00s <- GWP %>% filter(Year < 2010) %>% group_by(country) %>% summarise(
+  pre2010avg = mean(ls, na.rm = T)
+)
+GWP <- full_join(GWP, GWP00s)
+# Get scores after 2010
+# Separate between unhappy and happy
+GWP10s <- GWP %>% filter(Year >= 2010) %>% mutate(unhappy = pre2010avg < 5)
+
+GWP10s.mod1 <- lm(ls ~ unhappy*time, GWP10s)
+GWP10s.mod2 <- lm(log(ls) ~ unhappy*time, GWP10s)
+exp(coef(GWP10s.mod2))
+exp(coef(GWP10s.mod2)[3]+coef(GWP10s.mod2)[4])
+
+######################################
+# Calculating the effect on natives  #
+######################################
+
 # Effect on the natives #
 d.nativespillover <- read_sheet(
   "https://docs.google.com/spreadsheets/d/13s3TOFG8tlroWkJ_MjAYDyqs7KOj6ZMUREMTAr97W08/",
-  sheet = "Immigrate <> SWB (effects on host)"
+  sheet = "SWB effects on natives"
 )
 
 # Add d_se and modify attitude (to be 1% increases)
@@ -56,64 +89,13 @@ ggplot(d.nativespillover, aes(x=attitudes, y=`d adjusted`)) +
   geom_hline(yintercept = 0, linetype="dashed") +
   theme_cowplot()
 
-# Trying quadratic modelling
-# mod.nativespillover3 <- rma.mv(yi = `d adjusted`, V = d_se^2,
-#                                slab = Citation,
-#                                random =  ~1 | Citation,
-#                                mods = ~ attitudes*I(attitudes^2),
-#                                test = "t", method="REML",
-#                                data = d.nativespillover)
-# summary(mod.nativespillover3)
-# regplot(mod.nativespillover3)
-
-# Trying to look at size areas
-# ggplot(d.nativespillover, aes(x=`how big the area is`, y=`d adjusted`)) +
-#   geom_point(aes(color=Citation)) + 
-#   # geom_smooth() +
-#   geom_abline() +
-#   theme_cowplot()
-
-GWP <- read_csv("happiness-cantril-ladder.csv", show_col_types = FALSE) %>% 
-  rename(ls = `Life satisfaction in Cantril Ladder (World Happiness Report 2021)`,
-         country = Entity) %>% 
-  mutate(time = Year - 2005)
-
-# Gap between happy and unhappy countries
-
-# Get average before 2010
-GWP00s <- GWP %>% filter(Year < 2010) %>% group_by(country) %>% summarise(
-  pre2010avg = mean(ls, na.rm = T)
-)
-GWP <- full_join(GWP, GWP00s)
-# Get scores after 2010
-# Separate between unhappy and happy
-GWP10s <- GWP %>% filter(Year >= 2010) %>% mutate(unhappy = pre2010avg < 5)
-
-GWP10s.mod1 <- lm(ls ~ unhappy*time, GWP10s)
-GWP10s.mod2 <- lm(log(ls) ~ unhappy*time, GWP10s)
-exp(coef(GWP10s.mod2))
-exp(coef(GWP10s.mod2)[3]+coef(GWP10s.mod2)[4])
-
-# LS over time analysis
-summary(lm(ls ~ time, GWP))
-summary(lm(log(ls) ~ time, GWP))
-
-# SD of LS over time analysis
-GWP_sd <- GWP %>% group_by(Year) %>% 
-  summarise(sd = sd(ls, na.rm = T)) %>% 
-  mutate(time = Year - 2005)
-GWP.mod1 <- lm(sd ~ time, GWP_sd)
-summary(GWP.mod1)
-GWP.mod2 <- lm(log(sd) ~ time, GWP_sd)
-summary(GWP.mod2)
-exp(coef(GWP.mod2))
-
-
-
+####################################################
+# Calculating the effect on households left behind #
+####################################################
 
 hhspillovers <- read_sheet(
   "https://docs.google.com/spreadsheets/d/13s3TOFG8tlroWkJ_MjAYDyqs7KOj6ZMUREMTAr97W08/",
-  sheet = "Immigrate <> SWB (hh spillovers)"
+  sheet = "SWB hh spillovers"
 )
 
 hhspillovers <- hhspillovers %>% rowwise() %>% mutate(
@@ -128,6 +110,3 @@ mod.hhspillovers1 <- rma.mv(yi = d, V = d_se^2,
                                test = "t", method="REML",
                                data = hhspillovers)
 summary(mod.hhspillovers1)
-
-# hhspillovers <- hhspillovers %>% rename(N = `sample size`, gap = `% of gap closed`)
-# GAP.hh <- weighted.mean(x = hhspillovers$gap, w = hhspillovers$N, na.rm = T); GAP.hh*100
